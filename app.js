1e6607eb118df8a7d3656c2fad7d3184b5286d03
vivekj07 <jadhavvivek2604@gmail.com>
2024-08-25 01:27:53 +0530

    cors problem

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ const server = createServer(app)
 export const io = new Server(server, {
     cors: {
         origin: ['http://localhost:5173', 'http://localhost:4173', process.env.CLIENT_URL],
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
 })
